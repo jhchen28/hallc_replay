ecb0cd49722de46075276528796141c78fb4c14f
Murchhana Roy <mroy@ifarm1802.jlab.org>
2020-05-20 15:14:43 -0400

    Fixed typo in hms replay script:h_fadc_debug_param

--- SCRIPTS/src/replay_hms.cxx
+++ SCRIPTS/src/replay_hms.cxx
@@ -99,8 +99,9 @@ int replay_hms(
   gHcParms->Load(gHcParms->GetString("g_ctp_kinematics_filename"), RunNumber);
   // Load parameters for HMS trigger configuration
   gHcParms->Load("PARAM/TRIG/thms.param");
+
   // Load fadc debug parameters
-  gHcParms->Load("PARAM/HMS/GEN/p_fadc_debug.param");
+  gHcParms->Load("PARAM/HMS/GEN/h_fadc_debug.param");
 
   // Load the Hall C detector map
   gHcDetectorMap = new THcDetectorMap();
